0174a920b4e2707df851614a540f5f676775ec8b
Shivam <shivam142002@gmail.com>
2026-02-07 13:29:15 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './App/Store.jsx'
+import { store } from './app/Store.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
